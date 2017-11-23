c1513da41362d8e953fd6756331bcd093e334568
Gabriela <gabriela@terra.com.br>
2017-11-23 19:25:37 -0200

    Primeiro Evento

---
+++ carro.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Threading;
+
+namespace ExemploEvento
+{
+    public class Carro
+    {
+        int qtdTanque = 0;
+
+        public Carro(int qtdGasolina)
+        {
+            this.qtdTanque = qtdGasolina;            
+        }
+
+        public void Ligar()
+        {
+            while(true){
+                Console.WriteLine("Tanque: "+this.qtdTanque);
+                Thread.Sleep(1000); //thread.sleep entra no laço, espera o tempo de mili segundos e executa a função
+                this.qtdTanque--;
+
+                if(this.qtdTanque.Equals(3)){
+                    this.Ligacao("11984981497");
+                }
+
+                if(this.qtdTanque.Equals(0)){
+                    this.TanqueVazio(); //TanqueVazio é o evento do carro quando o tanque chegar a zero                 
+                    break;
+                }
+            }
+        }
+
+        public delegate void EventoCarro();
+
+        public event EventoCarro TanqueVazio;
+
+        public delegate void Chamar (string texto);
+        
+        public event Chamar Ligacao;
+    }
+}
